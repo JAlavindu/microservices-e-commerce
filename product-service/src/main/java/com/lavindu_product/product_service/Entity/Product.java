f271ec3e6e39cee2321ec3a906f99745b9af6f60
lavindu perera <lavi156perera@gmail.com>
2026-04-04 20:14:12 +0530

    added product entity

---
+++ product-service/src/main/java/com/lavindu_product/product_service/Entity/Product.java
@@ -0,0 +1,30 @@
+package com.lavindu_product.product_service.Entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name="products")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private String skuCode;
+    
+}
