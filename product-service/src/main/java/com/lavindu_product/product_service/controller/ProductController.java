7c179f3e4673ecf88f5349e85df4a30a09f50ae4
JAlavindu <lavi156perera@gmail.com>
2026-04-11 20:15:17 +0530

    setup of inventory service

--- product-service/src/main/java/com/lavindu_product/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/lavindu_product/product_service/controller/ProductController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.lavindu_product.product_service.DTO.ProductRequestDto;
 import com.lavindu_product.product_service.DTO.ProductResponseDto;
-import com.lavindu_product.product_service.Entity.Product;
-import com.lavindu_product.product_service.repository.ProductRepository;
 import com.lavindu_product.product_service.service.ProductService;
 
 import jakarta.validation.Valid;
@@ -17,12 +15,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 
 
 
