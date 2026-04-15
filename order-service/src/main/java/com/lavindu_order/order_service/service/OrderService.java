b106ac49f5e39c56d64554ed44ce519655117183
JAlavindu <lavi156perera@gmail.com>
2026-04-15 20:17:34 +0530

    added kafka messaging to order

--- order-service/src/main/java/com/lavindu_order/order_service/service/OrderService.java
+++ order-service/src/main/java/com/lavindu_order/order_service/service/OrderService.java
@@ -5,14 +5,18 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.kafka.core.KafkaTemplate;
 
 import com.lavindu_order.order_service.dto.InventoryResponseDTO;
 import com.lavindu_order.order_service.dto.OrderRequestDTO;
 import com.lavindu_order.order_service.dto.OrderResponseDTO;
 import com.lavindu_order.order_service.entity.Order;
+import com.lavindu_order.order_service.event.OrderPlacedEvent;
 import com.lavindu_order.order_service.feign.InventoryClient;
 import com.lavindu_order.order_service.mapper.OrderMapper;
 import com.lavindu_order.order_service.repository.OrderRepository;
+
+
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,6 +27,7 @@ public class OrderService {
     
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public OrderResponseDTO placeOrder(OrderRequestDTO orderRequestDTO){
         orderRequestDTO.setOrderNumber(UUID.randomUUID().toString());
@@ -42,6 +47,8 @@ public class OrderService {
 
         if (allProductsInStock && !skuCodes.isEmpty()) {
             Order savedOrder = orderRepository.save(order);
+
+            kafkaTemplate.send("order-events", new OrderPlacedEvent(savedOrder.getOrderNumber()));
             
             // 6. Map back to Response DTO
             return orderMapper.toResponseDto(savedOrder);
